ef0c0d17d0fbaafcf7f6cd040fae0c2c95e41554
Vladimir Mitev <vmitev2001@abv.bg>
2024-02-19 23:40:10 +0200

    Add task two from lab

---
+++ Recursion and Backtracking/Recursion and Backtracking/02. Recursive Drawing/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _02._Recursive_Drawing
+{
+    using System;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine()!);
+
+            DrawRecursivley(n);
+        }
+
+        private static void DrawRecursivley(int n)
+        {
+            if (n == 0)
+            {
+                return;
+            }
+
+            Console.WriteLine(new string('*', n));
+
+            DrawRecursivley(n - 1);
+
+            Console.WriteLine(new string('#', n));
+
+            n++;
+        }
+    }
+}
