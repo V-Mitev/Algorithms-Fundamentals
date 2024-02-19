e72128f6f27665e9019402e023b86f2b4c91fb04
Vladimir Mitev <vmitev2001@abv.bg>
2024-02-20 00:45:22 +0200

    Add task three from lab

---
+++ Recursion and Backtracking/Recursion and Backtracking/03. Generating Vectors/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _03._Generating_Vectors
+{
+    using System;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine()!);
+
+            int[] array = new int[n];
+
+            PrintVecotr(array, 0);
+        }
+
+        private static void PrintVecotr(int[] array, int index)
+        {
+            if (index == array.Length)
+            {
+                Console.WriteLine(string.Join(string.Empty, array));
+
+                return;
+            }
+
+            for (int i = 0; i < 2; i++)
+            {
+                array[index] = i;
+
+                PrintVecotr(array, index + 1);
+            }
+        }
+    }
+}
