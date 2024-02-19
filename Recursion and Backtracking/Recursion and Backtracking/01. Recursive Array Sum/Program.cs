a921af4babc1722e13ce670b1655b2010556004f
Vladimir Mitev <vmitev2001@abv.bg>
2024-02-19 23:26:59 +0200

    Add task one from lab

---
+++ Recursion and Backtracking/Recursion and Backtracking/01. Recursive Array Sum/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _01._Recursive_Array_Sum
+{
+    using System;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var input = Console.ReadLine()!
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            Console.WriteLine(CalculateArraySumRecursivley(input, 0));
+        }
+
+        private static int CalculateArraySumRecursivley(int[] input, int index)
+        {
+            if (index == input.Length - 1)
+            {
+                return input[index];
+            }
+
+            return input[index] + CalculateArraySumRecursivley(input, index + 1);
+        }
+    }
+}
