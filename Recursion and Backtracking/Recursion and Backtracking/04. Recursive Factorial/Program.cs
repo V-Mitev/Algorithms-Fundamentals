9a61fda7b68fb8db230a47024cfe4fd7bdb43ff9
Vladimir Mitev <vmitev2001@abv.bg>
2024-02-20 12:36:29 +0200

    Add task four from lab

---
+++ Recursion and Backtracking/Recursion and Backtracking/04. Recursive Factorial/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _04._Recursive_Factorial
+{
+    using System;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = int.Parse(Console.ReadLine()!);
+
+            Console.WriteLine(CalculateFactorialRecursivley(number));
+        }
+
+        private static int CalculateFactorialRecursivley(int number)
+        {
+            if (number == 0)
+            {
+                return 1;
+            }
+
+            return number * CalculateFactorialRecursivley(number - 1);
+        }
+    }
+}
